487509d9c8674b661edc30fb6cac339f73f7f18b
AJericSixteen <154009866+AJericSixteen@users.noreply.github.com>
2025-02-03 21:19:12 +0800

    Initial commit

---
+++ assets/database/db.php
@@ -0,0 +1,9 @@
+<?php
+// Replace with actual database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "iii";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
---
+++ iiisystem/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>III Advertising Services</title>
+    <link rel="stylesheet" href="./asset/css/style.css">
+    <link rel="icon" href="./asset/img/logo.png">
+</head>
+<body>
+    <div class="login-container">
+        <form class="login-form">
+
+                <img src="./asset/img/logo.png" alt="logo" width=100>
+
+            <div class="input-group">
+                <label for="username">Username:</label>
+                <input type="username" id="username" placeholder="Username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" placeholder="Enter your password" required>
+            </div>
+            <button type="submit" class="login-btn"><p class="login-txt">Login</p></button>
+        </form>
+    </div>
+</body>
+</html>
