b89065d57cf8fff4f9f4b5ad8149fe7c469d5881
AJericSixteen <154009866+AJericSixteen@users.noreply.github.com>
2025-02-07 17:16:45 +0800

    Update index.php

--- iiisystem/index.php
+++ iiisystem/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +8,12 @@
     <link rel="stylesheet" href="./asset/css/style.css">
     <link rel="icon" href="./asset/img/logo.png">
 </head>
+
 <body>
     <div class="login-container">
         <form class="login-form">
 
-                <img src="./asset/img/logo.png" alt="logo" width=100>
+            <img src="./asset/img/logo.png" alt="logo" width=100>
 
             <div class="input-group">
                 <label for="username">Username:</label>
@@ -21,8 +23,11 @@
                 <label for="password">Password</label>
                 <input type="password" id="password" placeholder="Enter your password" required>
             </div>
-            <button type="submit" class="login-btn"><p class="login-txt">Logins</p></button>
+            <button type="submit" class="login-btn">
+                <p class="login-txt">Logins</p>
+            </button>
         </form>
     </div>
 </body>
+
 </html>
